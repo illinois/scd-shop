d6f48e06f7cf863cb3a720c11234fc2ffd1f35a5
Rishi Mohan <rishinm2@illinois.edu>
2023-03-06 19:14:56 -0600

    developing query and file-reader functions

--- backend/app.py
+++ backend/app.py
@@ -11,36 +11,100 @@ import requests
 import pandas as pd
 import json
 from dash import Dash, html, dcc
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import numpy as np
 
+# read secrets
 
-r = requests.get('http://128.174.109.12/api/report/trigger/all/runtime/yesterday', 
-           data = {'auth_token': 'grit_a9448704457346bf95eb4e27b66d2bd0'})
+keys = open('keys.txt', 'r')
+auths = keys.readlines()
+auth_token = auths[0].strip()
+bearer_token = auths[1].strip()
+URL = auths[2].strip()
+keys.close()
 
-content = json.loads(r.text)[:2]
+### query API function ###
 
-machineData = pd.DataFrame.from_records(content[0])
-userData = pd.DataFrame.from_records(content[1])
+def queryGritData(timeframe = 'yesterday', reportType = 'toolReport', authToken = auth_token, bearerToken = bearer_token, URL = URL):
+    
+    # make a dictionary for each of the endpoint lookups using the timeframe var
+    
+    endpointDict = {'fullReport': f'{URL}/report/activity/{timeframe}/',
+                    'runtimeReport': f'{URL}/report/activity/{timeframe}/runtime/',
+                    'trackReport': f'{URL}/report/activity/{timeframe}/track/',
+                    'statusReport': f'{URL}/report/activity/{timeframe}/deviceStatus/',
+                    'signonReport': f'{URL}/report/signon/{timeframe}/',
+                    'assetReport': f'{URL}/report/asset/{timeframe}/',
+                    'toolReport': f'{URL}/report/trigger/all/runtime/{timeframe}/',
+                    'userReport': f'{URL}/report/user/all/runtime/{timeframe}/'}
+    
+    r = requests.get(endpointDict[f"{reportType}"], 
+               data = {'auth_token': auth_token})
 
-app = Dash(__name__)
+    # save raw data
+    
+    content = json.loads(r.text)
+    content.append(f'{reportType}')
+    dump = json.dumps(content)
+    with open(f'data/{reportType}_{timeframe}.json', 'w') as output:
+        output.write(dump)
+    
+    return(content)
 
+def dataReader(timeframe = 'yesterday', reportType = 'toolReport'):
+    try:
+        file = open(f'data/{reportType}_{timeframe}.json', 'r')
+        content = pd.read_json(file)
+        if(content[-1] == 'toolReport'):
+            # machine & user
+        elif(content[-1] == 'userReport'):
+            # user total & user by-tool
+        else:
+            # all others are 1 layer
+    except FileNotFoundError:
+        content = queryGritData(timeframe, reportType)
+    except:
+        print('oh shit lol')
+        exit(-1)
+    
+        
+    return()
+
+reportType = 'toolReport'
+timeframe = 'yesterday'
+
+file = open(f'data/{reportType}_{timeframe}.json', 'r')
+content = pd.read_json(file)
+
+
+
+
+
+###
+    
+content = queryGritData()
+
+machineData = content[1]
+userData = content[2]
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.SPACELAB], title = 'Test Application')
 
 fig1 = px.bar(machineData, x = 'deviceName', y = 'value', color = 'deviceName')
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(
+        children='Test Application'),
 
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
+    html.Div(children='Dash: A web application framework for your data.'),
 
     dcc.Graph(
-        id='example-graph',
+        id='example-graph-2',
         figure=fig1
     )
 ])
+
              
-if __name__ == '__main__':
-    app.run_server(debug=False)
+# if __name__ == '__main__':
+#     app.run_server(debug=False)
 
