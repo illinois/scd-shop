bdd188513cc99218944f61a1e0f3e41e7d34c19c
Rishi Mohan <rishinm2@illinois.edu>
2023-03-07 01:06:28 -0600

    tested dropdown-timeframe plot

--- backend/app.py
+++ backend/app.py
@@ -9,23 +9,31 @@ from funcs import *
 
 machineData, userData = getData()
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.SPACELAB], title = 'SCD Shop Dashboard')
+app = Dash(__name__, external_stylesheets=[
+           dbc.themes.SPACELAB], title='SCD Shop Dashboard')
 
-fig1 = px.bar(machineData, x = 'deviceName', y = 'value', color = 'deviceName')
-
-app.layout = html.Div(children=[
-    html.H1(
-        children='Test Application'),
-
+app.layout = html.Div([
+    html.H1('Test Application'),
     html.Div(children='Woah, a subtitle!'),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig1
-    )
+    dcc.Dropdown( options = timeframeDict,
+        value = 'yesterday',
+        id = 'timeframe-dropdown'),
+    dcc.Graph(id = 'machineData-figure')
 ])
 
-             
+@app.callback(
+    Output('machineData-figure', 'figure'),
+    Input('timeframe-dropdown', 'value'))
+
+def update_figure(selected_timeframe):
+    machineData, userData = getData(timeframe = selected_timeframe)
+    
+    fig = px.bar(machineData, x='deviceName', y='value', color='deviceName')
+    
+    fig.update_layout(transition_duration = 250)
+    
+    return fig
+
+
 if __name__ == '__main__':
     app.run_server(debug=False)
-
--- backend/funcs.py
+++ backend/funcs.py
@@ -9,7 +9,7 @@ Created on Mon Mar  6 23:52:23 2023
 import requests
 import pandas as pd
 import json
-from dash import Dash, html, dcc
+from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import numpy as np
@@ -17,6 +17,20 @@ from dotenv import load_dotenv
 import os
 
 # read secrets
+
+timeframeDict = {'today': 'Today',
+    'yesterday': 'Yesterday',
+    'this-week': 'This Week',
+    'last-week': 'Last Week',
+    'this-month': 'This Month',
+    'last-month': 'Last Month',
+    'last-7': 'Last 7 Days',
+    'last-14': 'Last 14 Days',
+    'last-30': 'Last 30 Days',
+    'last-60': 'Last 60 Days',
+    'this-year': 'This Year',
+    'all-time': 'All Time'}
+
 def secretFunc():
     load_dotenv()
     global auth_token
@@ -25,12 +39,16 @@ def secretFunc():
     auth_token = os.getenv('auth_token')
     bearer_token = os.getenv('bearer_token')
     URL = os.getenv('URL')
-    
+
+
+# load secrets
 secretFunc()
-    
+
 # query relevant GRIT endpoint
-def queryGritData(timeframe = 'yesterday', reportType = 'toolReport', authToken = auth_token, bearerToken = bearer_token, URL = URL):
-    
+
+
+def queryGritData(timeframe='yesterday', reportType='toolReport', authToken=auth_token, bearerToken=bearer_token, URL=URL):
+
     # make a dictionary for each of the endpoint lookups using the timeframe var
     endpointDict = {'fullReport': f'{URL}/report/activity/{timeframe}/',
                     'runtimeReport': f'{URL}/report/activity/{timeframe}/runtime/',
@@ -40,14 +58,14 @@ def queryGritData(timeframe = 'yesterday', reportType = 'toolReport', authToken 
                     'assetReport': f'{URL}/report/asset/{timeframe}/',
                     'toolReport': f'{URL}/report/trigger/all/runtime/{timeframe}/',
                     'userReport': f'{URL}/report/user/all/runtime/{timeframe}/'}
-    
-    r = requests.get(endpointDict[f"{reportType}"], 
-               data = {'auth_token': auth_token})
+
+    r = requests.get(endpointDict[f"{reportType}"],
+                     data={'auth_token': auth_token})
 
     # save raw data
-    
+
     content = json.loads(r.text)
-    
+
     if(reportType == 'toolReport'):
         machineData = pd.read_json(json.dumps(content[0]))
         userData = pd.read_json(json.dumps(content[1]))
@@ -58,7 +76,7 @@ def queryGritData(timeframe = 'yesterday', reportType = 'toolReport', authToken 
         json.dump(userData.to_dict(), file2)
         file2.close()
         return(machineData, userData)
-    
+
     elif(reportType == 'userReport'):
         userTotal = pd.read_json(json.dumps(content[0]))
         userTools = pd.read_json(json.dumps(content[1]))
@@ -69,60 +87,60 @@ def queryGritData(timeframe = 'yesterday', reportType = 'toolReport', authToken 
         json.dump(userTools.to_dict(), file2)
         file2.close()
         return(userTotal, userTools)
-    
+
     else:
         file = open(f'data/{reportType}_{timeframe}.json', 'w')
         json.dump(content, file)
         file.close()
         return(content)
-   
 
 # basic data retrieval - checks for local copy then pulls from API if necessary
 # implement stale-data checking or just have manual refresh
-def getData(timeframe = 'yesterday', reportType = 'toolReport'):
-    
+
+
+def getData(timeframe='yesterday', reportType='toolReport'):
+
     if reportType == 'toolReport':
-        
+
         try:
             file1 = open(f'data/machineData_{timeframe}.json', 'r')
             machineData = json.load(file1)
             file1.close()
-            file2 = open(f'data/userData_{timeframe}.json','r')
+            file2 = open(f'data/userData_{timeframe}.json', 'r')
             userData = json.load(file2)
             file2.close()
-            
+
             return(machineData, userData)
-        
+
         except:
             machineData, userData = queryGritData(timeframe, reportType)
             return(machineData, userData)
-            
+
     if reportType == 'userReport':
-        
+
         try:
             file1 = open(f'data/machineData_{timeframe}.json', 'r')
             userTotal = json.load(file1)
             file1.close()
-            file2 = open(f'data/userData_{timeframe}.json','r')
+            file2 = open(f'data/userData_{timeframe}.json', 'r')
             userTools = json.load(file2)
             file2.close()
-            
+
             return(userTotal, userTools)
-        
+
         except:
             userTotal, userTools = queryGritData(timeframe, reportType)
             return(userTotal, userTools)
-            
+
     else:
-        
+
         try:
             file = open(f'data/{reportType}_{timeframe}.json', 'r')
             data = json.load(file)
             file.close()
-            
+
             return(data)
-        
+
         except FileNotFoundError:
             data = queryGritData(timeframe, reportType)
             return(data)
-        
